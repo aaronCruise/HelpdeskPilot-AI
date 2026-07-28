429fa97941b1137e4699ebafcb783aa958e3dcc1
Aaron Cruz <aaron.m.cruz@rutgers.edu>
2026-07-28 15:33:29 +0000

    Implement creating db for RAG

--- app/services/rag_service.py
+++ app/services/rag_service.py
@@ -1,4 +1,5 @@
 from app.main import DOCUMENTS
+from chromadb import Client
 
 # Split document lines into chunks with metadata
 def chunk_documents(documents=DOCUMENTS) -> list:
@@ -10,12 +11,34 @@ def chunk_documents(documents=DOCUMENTS) -> list:
                 continue
             chunks.append(
                 {
-                    'source': document['filename'],
-                    'chunk_index': chunk_index,
-                    'chunk': line,
+                    'metadata': {
+                        'source': document['filename'],
+                        'chunk_index': chunk_index
+                    },
+                    'text': line
                 }
             )
             chunk_index += 1
         chunk_index = 0
-        
+
     return chunks
+
+# TODO: possibly persistent memory for db
+def create_collection():
+    chunks = chunk_documents()
+    chroma_client = Client()
+    collection = chroma_client.create_collection(name="knowledge_base")
+
+    ids = [
+        f'{chunk['metadata']['source'].removesuffix('.md')}_{chunk['metadata']['chunk_index']}' 
+        for chunk in chunks
+        ]
+    documents = [chunk['text'] for chunk in chunks]
+    metadatas = [chunk['metadata'] for chunk in chunks]
+
+    collection.add(
+        ids=ids,
+        documents=documents,
+        metadatas=metadatas
+    )
+    return collection
